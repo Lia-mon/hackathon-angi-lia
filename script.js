2f60a4ed7c711f1646b896d44f51abbd53feb9eb
Lia Ntikou <antasimis@gmail.com>
2023-05-05 22:11:43 +0300

    Adding a drawing script for the grid

---
+++ script.js
@@ -0,0 +1,9 @@
+const gridw = document.getElementById("gridw").value
+const gridh = document.getElementById("gridh").value
+
+const canvas = document.getElementById("canvas");
+if (canvas.getContext){
+  const ctx = canvas.getContext("2d");
+
+  
+}
