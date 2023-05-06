f1c3fce847cf00a753176dec84512c4b6610bc75
Lia Ntikou <antasimis@gmail.com>
2023-05-06 03:29:27 +0300

    More UI thingies

--- script.js
+++ script.js
@@ -58,3 +58,20 @@ function printGrid(){
 
   gridS.innerHTML = darray.map( x=>x.join("") ).join(" ");
 }
+
+function updateSize(){
+  const new_size = document.getElementById("c_size").value;
+  const r = document.querySelector(':root');
+
+  r.style.setProperty("--cell-size",new_size.toString() +"px");
+  r.style.setProperty("--cell-margin",Math.round(0.16*new_size).toString() +"px");
+  r.style.setProperty("--cell-font",Math.round(0.45*new_size).toString() +"px");
+}
+
+function toggle(source) {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i] != source)
+          checkboxes[i].checked = source.checked;
+  }
+}
